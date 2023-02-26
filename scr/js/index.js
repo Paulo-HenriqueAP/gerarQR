d3667fc0619115576ec4ecee0da55e38f51498e9
Paulo-HenriqueAP <123279522+Paulo-HenriqueAP@users.noreply.github.com>
2023-02-26 11:04:04 -0300

    Add files via upload

---
+++ scr/js/index.js
@@ -0,0 +1,25 @@
+let botaoGerar = document.querySelector(".gerar");
+let botaoVoltar = document.querySelector(".gerar_anterior")
+var qrCode = document.getElementById("gerador");
+var sequenciaFinal = 0;
+let pressionado = false; 
+
+botaoGerar.addEventListener("click", function() {
+    if (!pressionado) {
+        qrCode.setAttribute("src", "https://image-charts.com/chart?chs=252x252&cht=qr&chl= " + "0221512070160715" + sequenciaFinal + "&choe=UTF-8");
+        sequenciaFinal = sequenciaFinal + 1;
+        pressionado = true;
+        } else {
+            pressionado = false;
+        }
+});
+
+botaoVoltar.addEventListener("click", function() {
+    if (!pressionado) {
+        sequenciaFinal = sequenciaFinal - 1;
+        qrCode.setAttribute("src", "https://image-charts.com/chart?chs=252x252&cht=qr&chl= " + "0221512070160715" + sequenciaFinal + "&choe=UTF-8");
+        pressionado = true;
+        } else {
+            pressionado = false;
+        }
+});
