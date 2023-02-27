80217d5c164d2cbc12ba4928b1c0a95adaf56233
Paulo-HenriqueAP <123279522+Paulo-HenriqueAP@users.noreply.github.com>
2023-02-27 19:47:50 -0300

    Update index.js

--- scr/js/index.js
+++ scr/js/index.js
@@ -1,31 +1,39 @@
 let botaoGerar = document.querySelector(".gerar");
-let botaoVoltar = document.querySelector(".gerar_anterior")
-var qrCode = document.getElementById("gerador");
-var sequenciaFinal = 0;
+let botaoVoltar = document.querySelector(".gerar_anterior");
+let qrCode = document.getElementById("gerador");
+let sequenciaFinal = 0;
+let contagemFichas = 0;
+let adicionarMais = document.querySelector(".totalFichas");
+let idFicha = document.querySelector(".idFicha")
 let pressionado = false;
-var contagemFichas = 0
-let adicionarMais = document.querySelector(".totalFichas")
 
-botaoGerar.addEventListener("click", function() {
-    if (!pressionado) {
-        contagemFichas = contagemFichas + 1;
-        adicionarMais.textContent = "Fichas geradas: " + contagemFichas;
-        qrCode.setAttribute("src", "https://image-charts.com/chart?chs=252x252&cht=qr&chl= " + "0221512070160715" + sequenciaFinal + "&choe=UTF-8");
-        sequenciaFinal = sequenciaFinal + 1;
-        pressionado = true;
-        } else {
-            pressionado = false;
-        }
+pressionado = false;
+
+botaoGerar.addEventListener("mousedown", function() {
+    pressionado = true;
+    contagemFichas = contagemFichas + 1;
+    sequenciaFinal = sequenciaFinal + 1;
+    adicionarMais.textContent = "Fichas geradas: " + contagemFichas;
+    qrCode.setAttribute("src", "https://image-charts.com/chart?chs=252x252&cht=qr&chl= " + "0221512070160715" + sequenciaFinal + "&choe=UTF-8");
+    idFicha.textContent = "ID: 02215120701607150" + sequenciaFinal;
 });
 
-botaoVoltar.addEventListener("click", function() {
-    if (!pressionado) {
-        contagemFichas = contagemFichas - 1;
-        adicionarMais.textContent = "Fichas geradas: " + contagemFichas;
-        sequenciaFinal = sequenciaFinal - 1;
-        qrCode.setAttribute("src", "https://image-charts.com/chart?chs=252x252&cht=qr&chl= " + "0221512070160715" + sequenciaFinal + "&choe=UTF-8");
-        pressionado = true;
-        } else {
-            pressionado = false;
-        }
+botaoGerar.addEventListener("mouseup", function() {
+    pressionado = false;
+});
+
+botaoVoltar.addEventListener("mousedown", function() {
+    pressionado = true;
+    contagemFichas = contagemFichas - 1;
+    sequenciaFinal = sequenciaFinal - 1;
+    adicionarMais.textContent = "Fichas geradas: " + contagemFichas;
+    qrCode.setAttribute("src", "https://image-charts.com/chart?chs=252x252&cht=qr&chl= " + "0221512070160715" + sequenciaFinal + "&choe=UTF-8");
+    if (contagemFichas < 0) {
+        alert("Isso pode até funcionar, mas é melhor ir para frente.");
+        idFicha.textContent = "ID:02215120701607150" + sequenciaFinal;
+    }
+});
+
+botaoVoltar.addEventListener("mouseup", function() {
+    pressionado = false;
 });
