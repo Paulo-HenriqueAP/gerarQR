88d79260abf3d9220f0366ec032d91959124fbb6
Paulo-HenriqueAP <123279522+Paulo-HenriqueAP@users.noreply.github.com>
2023-03-01 20:54:45 -0300

    Update index.js

--- scr/js/index.js
+++ scr/js/index.js
@@ -6,18 +6,42 @@ let contagemFichas = 0;
 let adicionarMais = document.querySelector(".totalFichas");
 let idFicha = document.querySelector(".idFicha")
 let pressionado = false;
-
+let aleatorio = Math.floor(Math.random() * 90000 + 1001)
+const aleatorioLigado = document.getElementById("aleatoriON");
+const aleatorioDesligado = document.getElementById("aleatoriOFF");
+let aleatorioAtivo = true;
+let palavraAleatorio = document.getElementById("aleatorioPalavraOFF")
 pressionado = false;
 
-botaoGerar.addEventListener("mousedown", function() {
+aleatorioLigado.addEventListener("click", () => {
+    aleatorioAtivo = true;
+    aleatorioLigado.classList.add("hidden");
+    aleatorioDesligado.classList.remove("hidden");
+  });
+  
+  aleatorioDesligado.addEventListener("click", () => {
+    aleatorioAtivo = false;
+    aleatorioDesligado.classList.add("hidden");
+    aleatorioLigado.classList.remove("hidden");
+  });
+
+  botaoGerar.addEventListener("mousedown", function() {
     pressionado = true;
     contagemFichas = contagemFichas + 1;
-    sequenciaFinal = sequenciaFinal + 1;
     adicionarMais.textContent = "Fichas geradas: " + contagemFichas;
-    qrCode.setAttribute("src", "https://image-charts.com/chart?chs=252x252&cht=qr&chl= " + "0221512070160715" + sequenciaFinal + "&choe=UTF-8");
-    idFicha.textContent = "ID: 02215120701607150" + sequenciaFinal;
+    if (aleatorioAtivo) {
+        sequenciaFinal = sequenciaFinal + 1;
+        qrCode.setAttribute("src", "https://image-charts.com/chart?chs=252x252&cht=qr&chl= " + "0221512070160715" + sequenciaFinal + "&choe=UTF-8");
+        idFicha.textContent = "ID: 02215120701607150" + sequenciaFinal;
+    } else {
+        aleatorio = Math.floor(Math.random() * 90000+ 1001)
+        qrCode.setAttribute("src", "https://image-charts.com/chart?chs=252x252&cht=qr&chl= " + "022151207016" + aleatorio + "&choe=UTF-8");
+        idFicha.textContent = "ID: 022151207016" + aleatorio;
+    }
+    if (contagemFichas >= 0) {
+    palavraAleatorio.textContent = ""
+    };
 });
-
 botaoGerar.addEventListener("mouseup", function() {
     pressionado = false;
 });
@@ -31,6 +55,7 @@ botaoVoltar.addEventListener("mousedown", function() {
     if (contagemFichas < 0) {
         alert("Isso pode até funcionar, mas é melhor ir para frente.");
         idFicha.textContent = "ID:02215120701607150" + sequenciaFinal;
+        palavraAleatorio.textContent = ("Aleatório indisponível :(");
     }
 });
 
