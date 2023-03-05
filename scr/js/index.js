785407e44dca7e71a63b4fdbee74a8f322ade637
Paulo-HenriqueAP <paulo135657@gmail.com>
2023-03-05 15:44:10 -0300

    git commit -m "atts"

--- scr/js/index.js
+++ scr/js/index.js
@@ -3,24 +3,24 @@ let botaoVoltar = document.querySelector(".gerar_anterior");
 let qrCode = document.getElementById("gerador");
 let sequenciaFinal = 0;
 let contagemFichas = 0;
+let pressionado = false;
 let adicionarMais = document.querySelector(".totalFichas");
 let idFicha = document.querySelector(".idFicha")
-let pressionado = false;
 let aleatorio = Math.floor(Math.random() * 90000 + 1001)
 const aleatorioLigado = document.getElementById("aleatoriON");
 const aleatorioDesligado = document.getElementById("aleatoriOFF");
-let aleatorioAtivo = true;
-let palavraAleatorio = document.getElementById("aleatorioPalavraOFF")
+let aleatorioDesativado = true;
+let fichaAnterior = " ";
 pressionado = false;
 
 aleatorioLigado.addEventListener("click", () => {
-    aleatorioAtivo = true;
+    aleatorioDesativado = true;
     aleatorioLigado.classList.add("hidden");
     aleatorioDesligado.classList.remove("hidden");
   });
   
   aleatorioDesligado.addEventListener("click", () => {
-    aleatorioAtivo = false;
+    aleatorioDesativado = false;
     aleatorioDesligado.classList.add("hidden");
     aleatorioLigado.classList.remove("hidden");
   });
@@ -29,18 +29,17 @@ aleatorioLigado.addEventListener("click", () => {
     pressionado = true;
     contagemFichas = contagemFichas + 1;
     adicionarMais.textContent = "Fichas geradas: " + contagemFichas;
-    if (aleatorioAtivo) {
+    if (aleatorioDesativado) {
         sequenciaFinal = sequenciaFinal + 1;
+        fichaAnterior = sequenciaFinal - 1
         qrCode.setAttribute("src", "https://image-charts.com/chart?chs=252x252&cht=qr&chl= " + "0221512070160715" + sequenciaFinal + "&choe=UTF-8");
-        idFicha.textContent = "ID: 02215120701607150" + sequenciaFinal;
+        idFicha.textContent = "ID: 0221512070160715" + sequenciaFinal;
     } else {
+        fichaAnterior = aleatorio;
         aleatorio = Math.floor(Math.random() * 90000+ 1001)
         qrCode.setAttribute("src", "https://image-charts.com/chart?chs=252x252&cht=qr&chl= " + "022151207016" + aleatorio + "&choe=UTF-8");
         idFicha.textContent = "ID: 022151207016" + aleatorio;
     }
-    if (contagemFichas >= 0) {
-    palavraAleatorio.textContent = ""
-    };
 });
 botaoGerar.addEventListener("mouseup", function() {
     pressionado = false;
@@ -48,16 +47,14 @@ botaoGerar.addEventListener("mouseup", function() {
 
 botaoVoltar.addEventListener("mousedown", function() {
     pressionado = true;
-    contagemFichas = contagemFichas - 1;
-    sequenciaFinal = sequenciaFinal - 1;
-    adicionarMais.textContent = "Fichas geradas: " + contagemFichas;
-    qrCode.setAttribute("src", "https://image-charts.com/chart?chs=252x252&cht=qr&chl= " + "0221512070160715" + sequenciaFinal + "&choe=UTF-8");
-    if (contagemFichas < 0) {
-        alert("Isso pode até funcionar, mas é melhor ir para frente.");
-        idFicha.textContent = "ID:02215120701607150" + sequenciaFinal;
-        palavraAleatorio.textContent = ("Aleatório indisponível :(");
+    if (aleatorioDesativado) {
+        qrCode.setAttribute("src", "https://image-charts.com/chart?chs=252x252&cht=qr&chl= " + "0221512070160715" + fichaAnterior + "&choe=UTF-8")
+        idFicha.textContent = "ID: 0221512070160715" + fichaAnterior
+    } else {
+        qrCode.setAttribute("src", "https://image-charts.com/chart?chs=252x252&cht=qr&chl= " + "022151207016" + fichaAnterior + "&choe=UTF-8")
+        idFicha.textContent = "ID: 022151207016" + fichaAnterior
     }
-});
+    });
 
 botaoVoltar.addEventListener("mouseup", function() {
     pressionado = false;
