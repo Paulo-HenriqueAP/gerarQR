16bb4d5ea8886a059ff98ef086b4b42605bb6fed
Paulo-HenriqueAP <paulo135657@gmail.com>
2023-03-05 18:00:46 -0300

    Indisponível texto adicionado

--- scr/js/index.js
+++ scr/js/index.js
@@ -11,6 +11,7 @@ const aleatorioLigado = document.getElementById("aleatoriON");
 const aleatorioDesligado = document.getElementById("aleatoriOFF");
 let aleatorioDesativado = true;
 let fichaAnterior = " ";
+
 pressionado = false;
 
 aleatorioLigado.addEventListener("click", () => {
@@ -24,7 +25,7 @@ aleatorioLigado.addEventListener("click", () => {
     aleatorioDesligado.classList.add("hidden");
     aleatorioLigado.classList.remove("hidden");
   });
-
+  
   botaoGerar.addEventListener("mousedown", function() {
     pressionado = true;
     contagemFichas = contagemFichas + 1;
@@ -43,9 +44,16 @@ aleatorioLigado.addEventListener("click", () => {
 });
 botaoGerar.addEventListener("mouseup", function() {
     pressionado = false;
+    botaoVoltar.style.color = '';
+    botaoVoltar.textContent = "FICHA ANTERIOR";
 });
 
 botaoVoltar.addEventListener("mousedown", function() {
+    if (contagemFichas <= 0) {
+        botaoVoltar.style.color = '#b22222';
+        botaoVoltar.textContent = "Indisponível";
+        return;
+    }
     pressionado = true;
     if (aleatorioDesativado) {
         qrCode.setAttribute("src", "https://image-charts.com/chart?chs=252x252&cht=qr&chl= " + "0221512070160715" + fichaAnterior + "&choe=UTF-8")
